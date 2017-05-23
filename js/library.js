3e377b3b831e23f7c28fd05bff44d627ef8ab9c6
annemijn <annemijn.portier@hotmail.com>
2017-05-23 15:58:39 +0200

    Update library.js

--- js/library.js
+++ js/library.js
@@ -7,11 +7,15 @@ var library = {
  var bericht = '';
  var melding = document.getElementById('melding');   //zoek div die melding heet   
 var username = document.myform.username.value; //kijk wat de value is van de gebruikersnaam
+var isValidusername; 
+var isValidpassword;
+var isValidticket;
+
 	if (username == '') //als het leeg is
 	{
 		message = 'je moet wel je gebruikersnaam invullen<br>';
-		bericht += message;
 		document.myform.username.style.borderColor = 'red'; //maak een rood randje 
+		bericht += message;
 	}
 	else
 		{
@@ -19,7 +23,12 @@ var username = document.myform.username.value; //kijk wat de value is van de geb
 			if(!pattUsername.test(username))
 			{
 				message += '- De gebruikersnaam moet tussen de 3-15 karakters zijn met alleen de symbolen _-<br>';
+				document.myform.username.style.borderColor = 'red'; //maak een rood randje 
 				bericht += message;
+			}
+			else
+			{
+				isValidusername = true;
 			}
 		}
 	
@@ -36,18 +45,33 @@ var username = document.myform.username.value; //kijk wat de value is van de geb
 			if(!pattPassword.test(password))
 			{
 				message += '- Het wachtwoord moet een nummer, hoofdletter en meer dan zes karakters zijn.<br>';
+				document.myform.password.style.borderColor = 'red'; //maak een rood randje
 				bericht += message;
+			}
+			else
+			{
+				isValidpassword = true;
 			}
 		}
 
-			 var ddl = document.getElementById("tickets");
- var selectedValue = ddl.options[ddl.selectedIndex].value;
-    if (selectedValue == "selectcard")
+	var ddl = document.getElementById("tickets"); //zoek element met id tickets
+ 	var selectedValue = ddl.options[ddl.selectedIndex].value; //kijk wat de opties zijn van het select element en sla de values op
+    if (selectedValue == "selectcard") //als de value van de optie selectcard is
    {
 		message = 'Selecteer een ticket<br>';
 		document.myform.tickets.style.borderColor = 'red'; //maak een rood randje
 		bericht += message;
    }
+   else
+   {
+   	isValidticket = true;
+   }
+
+	var website = 'http://www.ticketmaster.nl/'; //maak variabele aan met link naar de website 
+       if(isValidpassword == true && isValidticket == true && isValidusername == true) //als alle gegevens goed zijn
+    {
+    	window.location.href = website; // ga naar de website
+    }
 	
 	if(message !== '') //als element niet leeg is
     {
